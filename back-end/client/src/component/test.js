b49640154a8b39f24a102e2da505d2d282ed78b8
huhuta <smarthorse6@gmail.com>
2018-10-25 17:22:14 +0900

    Add express starter

--- back-end/client/src/component/test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Test = () => {
-  return(
-    <P>Test Page</P>
-  )
-}
---
+++ util.js
@@ -0,0 +1,23 @@
+exports.logger = console;
+
+const { logger } = this;
+
+exports.catchErrors = (fn) => (req, res, next) =>
+  fn(req, res, next).catch(next);
+
+// eslint-disable-next-line no-unused-vars
+exports.developmentErrors = (err, req, res, next) => {
+  logger.error(err);
+  logger.error(err.message);
+  // logger.error(req);
+  res.status(err.status || 500);
+  res.send(JSON.stringify(err));
+};
+
+// eslint-disable-next-line no-unused-vars
+exports.productionErrors = (err, req, res, next) => {
+  logger.error(err);
+  // logger.error(req);
+  res.status(err.status || 500);
+  res.send(err.message);
+};
