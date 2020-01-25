54c67ce3ad14ca534c338faf82b0e2d033db15bb
yoyota <smarthorse6@gmail.com>
2020-01-25 23:11:49 +0900

    Minimize files (#1)
    * Minimize files
    
    * Add lint script

--- src/api/exampleCenter/exampleRouter.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router()
-const { catchErrors } = require("../../util")
-
-const { example, asyncExample } = require("./exampleCenter")
-
-router.get("/", example)
-router.get("/async", catchErrors(asyncExample))
-
-module.exports = router
--- src/app.js
+++ src/app.js
@@ -2,31 +2,20 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const compression = require("compression")
 const helmet = require("helmet")
-const cors = require("cors")
-const api = require("./api/index")
-const { logger, developmentErrors, productionErrors } = require("./util")
 
-const { NODE_ENV, PORT } = process.env
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(compression())
 app.use(helmet())
-app.use(cors())
 
-app.get("/", (req, res) => {
-  res.send("app home")
+app.get("/health", (_, res) => {
+  res.status(204).send()
 })
 
-app.use("/api", api)
-
-const errorHandler =
-  NODE_ENV === "production" ? productionErrors : developmentErrors
-app.use(errorHandler)
-
-app.listen(PORT || 5000, () => {
-  logger.info(`Server start! listening on ${PORT || 5000}`)
+app.get("/", (_, res) => {
+  res.send("home")
 })
 
 module.exports = app
